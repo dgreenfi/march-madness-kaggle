9f1404061a07d7fa9ecda4610c52fa0f6f0d7594
David Greenfield <dg2815@columbia.edu>
2016-03-03 11:43:33 -0500

    add more features

--- data_tests.py
+++ data_tests.py
@@ -24,28 +24,45 @@ def compile_team_metrics(seasons):
     return team_stats
 
 def calc_wins(seasons,team_stats,teams):
-    #prepopulate dict keys
-    years=range (syear,eyear)
-    for year in years:
-        for team in teams:
-            if team not in team_stats.keys():
-                team_stats[team]={}
-            if year not in team_stats[team].keys():
-                team_stats[team][year]={}
-    #create a numpy matrix for each team/year key
+    #create numpy matrix for population
     team_statmat=np.zeros([len(range(syear,eyear))*len(teams),50])
     c=0
     for year in range (syear,eyear):
         for team in teams:
-            #print team_stats
-            team_stats[team][year]['wins']=len(seasons[(np.where((seasons['Wteam']==team) & (seasons['Season'] ==year)))])
-            team_stats[team][year]['avgptsw']=np.mean((seasons[(np.where((seasons['Wteam']==team) & (seasons['Season'] ==year)))]['Wscore']))
-            team_stats[team][year]['avgptsl']=np.mean((seasons[(np.where((seasons['Lteam']==team) & (seasons['Season'] ==year)))]['Lscore']))
+            ############   Team Level Stats
             team_statmat[c,0]=team
             team_statmat[c,1]=year
+            # count of wins
             team_statmat[c,2]=len(seasons[(np.where((seasons['Wteam']==team) & (seasons['Season'] ==year)))])
-            team_statmat[c,3]=np.mean((seasons[(np.where((seasons['Wteam']==team) & (seasons['Season'] ==year)))]['Wscore']))
-            team_statmat[c,4]=np.mean((seasons[(np.where((seasons['Lteam']==team) & (seasons['Season'] ==year)))]['Lscore']))
+            #mean pts in wins
+            team_statmat[c,3]=np.mean((seasons[(np.where((seasons['Wteam']==team) & (seasons['Season']==year)))]['Wscore']))
+            #mean pts in losses
+            team_statmat[c,4]=np.mean((seasons[(np.where((seasons['Lteam']==team) & (seasons['Season']==year)))]['Lscore']))
+            #margin of victory in wins
+            team_statmat[c,5]=np.mean((seasons[(np.where((seasons['Wteam']==team) & (seasons['Season']==year)))]['Wscore'])-np.mean((seasons[(np.where((seasons['Wteam']==team) & (seasons['Season'] ==year)))]['Lscore'])))
+            #margin of loss in losses
+            team_statmat[c,6]=np.mean((seasons[(np.where((seasons['Lteam']==team) & (seasons['Season']==year)))]['Lscore'])-np.mean((seasons[(np.where((seasons['Lteam']==team) & (seasons['Season'] ==year)))]['Wscore'])))
+            #avg fgmade wins
+            team_statmat[c,7]=np.mean((seasons[(np.where((seasons['Wteam']==team ) & (seasons['Season']==year)))]['Wfgm']))
+            #avg fg attempted wins
+            team_statmat[c,8]=np.mean((seasons[(np.where((seasons['Wteam']==team ) & (seasons['Season']==year)))]['Wfga']))
+            #avg fgmade losses
+            team_statmat[c,9]=np.mean((seasons[(np.where((seasons['Lteam']==team ) & (seasons['Season']==year)))]['Lfgm']))
+            #avg fg attempted losses
+            team_statmat[c,10]=np.mean((seasons[(np.where((seasons['Lteam']==team ) & (seasons['Season']==year)))]['Lfga']))
+            #avg 3pt made wins
+            team_statmat[c,11]=np.mean((seasons[(np.where((seasons['Wteam']==team ) & (seasons['Season']==year)))]['Wfgm3']))
+            #avg 3pt attempted wins
+            team_statmat[c,12]=np.mean((seasons[(np.where((seasons['Wteam']==team ) & (seasons['Season']==year)))]['Wfgm3']))
+            #avg 3pt made losses
+            team_statmat[c,13]=np.mean((seasons[(np.where((seasons['Lteam']==team ) & (seasons['Season']==year)))]['Lfgm3']))
+            #avg 3pt attempted losses
+            team_statmat[c,14]=np.mean((seasons[(np.where((seasons['Lteam']==team ) & (seasons['Season']==year)))]['Lfgm3']))
+
+            ##########Opponent statistics##########
+            # need a different appliance since opponent stats will have to grab a game set for each opponent team and not 1 team
+
+            #record iterator
             c+=1
     np.savetxt("teamyear.csv", team_statmat, delimiter=",")
     return team_statmat
@@ -55,15 +72,17 @@ def calc_wins(seasons,team_stats,teams):
 
 
 def main():
+    derive_team_stats=1
     datadir='march-machine-learning-mania-2016-v1/'
 
     #season data
     #Season,Daynum,Wteam,Wscore,Lteam,Lscore,Wloc,Numot,Wfgm,Wfga,Wfgm3,Wfga3,Wftm,Wfta,Wor,Wdr,Wast,Wto,Wstl,Wblk,Wpf,Lfgm,Lfga,Lfgm3,Lfga3,Lftm,Lfta,Lor,Ldr,Last,Lto,Lstl,Lblk,Lpf
     seasons=load_data_num(datadir+'RegularSeasonDetailedResults.csv')
     teams=load_data(datadir+'Teams.csv')
-    teamstats=compile_team_metrics(seasons)
+    if derive_team_stats==1:
+        teamstats=compile_team_metrics(seasons)
 
-    print teamstats
+
     #print seasons[np.where(seasons['Wteam']==1104)]
 if '__name__'!='main':
     main()
