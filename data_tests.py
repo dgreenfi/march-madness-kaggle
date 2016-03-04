4bbe88a712fb9e0dc741cd6ac16abf246f7f5785
David Greenfield <dg2815@columbia.edu>
2016-03-03 21:23:29 -0500

    feature merge

--- data_tests.py
+++ data_tests.py
@@ -1,9 +1,10 @@
 import numpy as np
 import csv
 import networkx as nx
-#import pandas as pd
+import pandas as pd
 syear=2003
 eyear=2016
+years=range(syear,eyear)
 
 def load_data(filename):
     mat=[]
@@ -27,10 +28,15 @@ def compile_team_metrics(seasons):
 
 def calc_wins(seasons,team_stats,teams):
     #create numpy matrix for population
-    team_statmat=np.zeros([len(range(syear,eyear))*len(teams),50])
+    team_statmat=np.zeros([len(range(syear,eyear))*len(teams),17])
     c=0
+    names=['team','year','wins','winscore','lossscore','mvic','mloss','wfgm','wfga','lfgm','lfga','wfgm3','wfga3','lfga3','lfgm3','pr','pr_w']
+    #index by team year key
+    index_df=[]
     for year in range (syear,eyear):
         for team in teams:
+            index_df.append(str(int(team))+str(year))
+
             ############   Team Level Stats
             team_statmat[c,0]=team
             team_statmat[c,1]=year
@@ -55,19 +61,21 @@ def calc_wins(seasons,team_stats,teams):
             #avg 3pt made wins
             team_statmat[c,11]=np.mean((seasons[(np.where((seasons['Wteam']==team ) & (seasons['Season']==year)))]['Wfgm3']))
             #avg 3pt attempted wins
-            team_statmat[c,12]=np.mean((seasons[(np.where((seasons['Wteam']==team ) & (seasons['Season']==year)))]['Wfgm3']))
+            team_statmat[c,12]=np.mean((seasons[(np.where((seasons['Wteam']==team ) & (seasons['Season']==year)))]['Wfga3']))
             #avg 3pt made losses
             team_statmat[c,13]=np.mean((seasons[(np.where((seasons['Lteam']==team ) & (seasons['Season']==year)))]['Lfgm3']))
             #avg 3pt attempted losses
-            team_statmat[c,14]=np.mean((seasons[(np.where((seasons['Lteam']==team ) & (seasons['Season']==year)))]['Lfgm3']))
+            team_statmat[c,14]=np.mean((seasons[(np.where((seasons['Lteam']==team ) & (seasons['Season']==year)))]['Lfga3']))
 
             ##########Opponent statistics##########
             # need a different appliance since opponent stats will have to grab a game set for each opponent team and not 1 team
 
             #record iterator
             c+=1
-    np.savetxt("teamyear.csv", team_statmat, delimiter=",")
-    return team_statmat
+
+    team_statmat_df = pd.DataFrame(team_statmat, index=index_df, columns=names)
+    #np.savetxt("teamyear.csv", team_statmat, delimiter=",")
+    return team_statmat_df
 
 
 def create_schedule_graph(seasons,teams):
@@ -76,26 +84,48 @@ def create_schedule_graph(seasons,teams):
     t_lookup={int(t[0]):t[1] for t in teams[1:]}
     print teams
     teams =[int(t[0]) for t in teams[1:]]
-    G=nx.DiGraph()
-    G.add_nodes_from(teams)
-    G_w=G.copy()
-    games=seasons[np.where((seasons['Season']==2015))]
-    for game in games:
-        #add a directional endorsement from the losing team to winning team
-        G.add_weighted_edges_from([(game['Lteam'],game['Wteam'],1)])
-        # weight by win % squared
-        G_w.add_weighted_edges_from([(game['Lteam'],game['Wteam'],(game['Wscore']/game['Lscore'])**2)])
-    pr=nx.pagerank(G, alpha=0.9)
-    pr_w=nx.pagerank(G_w, alpha=0.9)
-    ranks=[]
-    ranks_w=[]
-    for r in pr:
-        ranks.append((t_lookup[r],pr[r]))
-    for r in pr_w:
-        ranks_w.append((t_lookup[r],pr_w[r]))
+    pr_hist={}
+    pr_w_hist={}
+    for year in years:
+        G=nx.DiGraph()
+        G.add_nodes_from(teams)
+        G_w=G.copy()
+        games=seasons[np.where((seasons['Season']==year))]
+
+        for game in games:
+            #add a directional endorsement from the losing team to winning team
+            G.add_weighted_edges_from([(game['Lteam'],game['Wteam'],1)])
+            # weight by win % squared
+            G_w.add_weighted_edges_from([(game['Lteam'],game['Wteam'],(game['Wscore']/game['Lscore'])**2)])
+        pr=nx.pagerank(G, alpha=0.9)
+        pr_w=nx.pagerank(G_w, alpha=0.9)
+        ranks=[]
+        ranks_w=[]
+        for r in pr:
+            ranks.append((t_lookup[r],pr[r]))
+        for r in pr_w:
+            ranks_w.append((t_lookup[r],pr_w[r]))
+        pr_hist[year]=pr
+        pr_w_hist[year]=pr_w
     sorted_pr = sorted(ranks, key=lambda tup: tup[1],reverse=True)
     sorted_pr_w = sorted(ranks_w, key=lambda tup: tup[1],reverse=True)
-    return sorted_pr,sorted_pr_w
+    return pr_hist,pr_w_hist
+
+def append_ranks(mat,pr,colname):
+
+    for year in pr.keys():
+        for team in pr[year].keys():
+            #print pr[year][team]*10000
+            if str(team)+str(year) in mat.axes[0]:
+                mat[colname][str(team)+str(year)]=pr[year][team]*100
+                #print colname,str(team)+str(year),pr[year][team]*100
+
+    return mat
+
+def assemble_train_mat(teamstats,tourneyseeds,tourneyslots,tourneyresults,targetyear):
+    #assemble matrix X of teamA,TeamB,stat1,stat2...statn,opponentstat and array of vectors y of result, teamascore,teambscore
+
+
 
 def main():
     derive_team_stats=1
@@ -105,11 +135,21 @@ def main():
     #Season,Daynum,Wteam,Wscore,Lteam,Lscore,Wloc,Numot,Wfgm,Wfga,Wfgm3,Wfga3,Wftm,Wfta,Wor,Wdr,Wast,Wto,Wstl,Wblk,Wpf,Lfgm,Lfga,Lfgm3,Lfga3,Lftm,Lfta,Lor,Ldr,Last,Lto,Lstl,Lblk,Lpf
     seasons=load_data_num(datadir+'RegularSeasonDetailedResults.csv')
     teams=load_data(datadir+'Teams.csv')
-    sorted_pr,sorted_pr_w=create_schedule_graph(seasons,teams)
+    tourneyresults=load_data(datadir+'TourneyCompactResults.csv')
+    tourneyseeds=load_data(datadir+'TourneySeeds.csv')
+    tourneyslots=load_data(datadir+'TourneySlots.csv')
+    pr,pr_w=create_schedule_graph(seasons,teams)
+    #print pr
+
     if derive_team_stats==1:
         teamstats=compile_team_metrics(seasons)
 
+    teamstats=append_ranks(teamstats,pr,'pr')
+    teamstats=append_ranks(teamstats,pr_w,'pr_w')
+    teamstats.to_csv('teamyear.csv', index=True, header=True, sep=',')
 
+    assemble_train_mat(teamstats,tourneyseeds,tourneyslots,tourneyresults,2015)
+    #print teamstats
     #print seasons[np.where(seasons['Wteam']==1104)]
 if '__name__'!='main':
     main()
